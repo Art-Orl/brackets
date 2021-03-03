4fcb3632a331b37f72d112d9fdfcfbc8ae0d25e3
Art-Orl <orlyuta95@mail.ru>
2021-03-03 12:26:03 +0300

    feat: add a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+    let b1 = [];
+    let b2 = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        b1.push(bracketsConfig[i][0]);
+        b2.push(bracketsConfig[i][1]);
+    }
+    let stack = [];
+    for (let j = 0; j < str.length; j++) {
+        if (b1.indexOf(str[j]) !== -1) {
+            if (
+                b1.indexOf(str[j]) === b2.indexOf(str[j]) &&
+                str[j] === stack[stack.length - 1]
+            ) {
+                stack.pop();
+            } else {
+                stack.push(str[j]);
+            }
+        } else if (
+            b2.indexOf(str[j]) !== -1 &&
+            b2.indexOf(str[j]) === b1.indexOf(stack[stack.length - 1])
+        ) {
+            stack.pop();
+        } else {
+            return false;
+        }
+    }
+    if (stack.length > 0) {
+        return false;
+    } else {
+        return true;
+    }
+};
